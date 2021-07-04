8d21ce99fa1f7137d4c7648e40c48438220a4a90
AnjaliPeter <74590192+AnjaliPeter@users.noreply.github.com>
2021-07-04 10:59:38 +0530

    Add files via upload

--- Frontend/src/app/header/header.component.ts
+++ Frontend/src/app/header/header.component.ts
@@ -1,1 +1,88 @@
+import { Component, OnInit } from '@angular/core';
 
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  title = "PlayStream";
+  userid:any;
+
+  alert(){
+    Swal.fire({
+      title: 'Please SignIn',
+      text: "You must SignIn to your PlayStream channel !!!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'SignIn',
+      cancelButtonText: 'Cancel'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // clicked SignIn
+      } 
+      else if (result.dismiss === Swal.DismissReason.cancel) {
+        // clicked Cancel
+      }
+    });
+  }
+
+  videos(){
+    this.userid = localStorage.getItem("userid");
+    if(this.userid == null)
+    {
+      this.alert();
+    }
+    else
+    {
+      this.router.navigate(["home/videos/category/AllCategories"]);
+    }
+  }
+
+  contact(){
+    this.userid = localStorage.getItem("userid");
+    if(this.userid == null)
+    {
+      this.alert();
+    }
+    else
+    {
+      this.router.navigate(["home/contact"]);
+    }
+  }
+
+  help(){
+    this.userid = localStorage.getItem("userid");
+    if(this.userid == null)
+    {
+      this.alert();
+    }
+    else
+    {
+      this.router.navigate(["home/help"]);
+    }
+  }
+
+  signOut(){
+    this.userid = localStorage.getItem("userid");
+    if(this.userid == null)
+    {
+      this.alert();
+    }
+    else
+    {
+      localStorage.removeItem("userid");
+      this.router.navigate(["/"]);
+    }
+  }
+  
+}
