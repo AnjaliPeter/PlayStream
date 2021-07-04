c1f79ffd15acfcaeb21a0067be500c0e3090371e
AnjaliPeter <74590192+AnjaliPeter@users.noreply.github.com>
2021-07-04 10:52:34 +0530

    Add files via upload

--- Frontend/src/app/body/body.component.ts
+++ Frontend/src/app/body/body.component.ts
@@ -1,1 +1,34 @@
+import { Component, OnInit } from '@angular/core';
 
+import { Router,ActivatedRoute } from '@angular/router';
+import { from } from 'rxjs';
+import { ChannelsService } from '../channels.service';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+
+export class BodyComponent implements OnInit {
+
+  constructor(private channelsService:ChannelsService,private activatedRoute:ActivatedRoute) { }
+
+  userid:any = localStorage.getItem("userid");
+
+  ngOnInit(): void {
+
+    if(this.userid == "admin@PlayStream")
+    {
+      $("#channel-sec").hide();
+      $("#admin-sec").show();
+    }
+    else
+    {
+      $("#channel-sec").show();
+      $("#admin-sec").hide();
+    }
+
+  }
+
+}
