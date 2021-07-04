58b3195db4177707e70de25ec8591e3a5e003469
AnjaliPeter <74590192+AnjaliPeter@users.noreply.github.com>
2021-07-04 11:09:47 +0530

    Add files via upload

--- Frontend/src/app/videos/videos.component.ts
+++ Frontend/src/app/videos/videos.component.ts
@@ -1,1 +1,17 @@
+import { Component, OnInit } from '@angular/core';
 
+import { VideosService } from '../videos.service';
+
+@Component({
+  selector: 'app-videos',
+  templateUrl: './videos.component.html',
+  styleUrls: ['./videos.component.css']
+})
+export class VideosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
